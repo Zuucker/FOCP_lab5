7f60d1d380c011d97589f786caf2dfb46c58dea5
Zuucker <kamigie504@student.polsl.pl>
2020-11-05 12:00:48 +0100

    reading i/o flags

--- application/application.cpp
+++ application/application.cpp
@@ -6,6 +6,16 @@ int main(int argc, char *argv[])
     cout << argc << endl;
     for (int i = 0; i < argc; i++)
     {
-        cout << argv[i] << endl;
+        string input_file, output_file;
+        if (string(argv[i]) == "-i")
+        {
+            input_file = argv[i + 1];
+        }
+        if (string(argv[i]) == "-o")
+        {
+            output_file = argv[i + 1];
+        }
+        cout << "input file: " << input_file << endl;
+        cout << "output file: " << output_file << endl;
     }
 }
