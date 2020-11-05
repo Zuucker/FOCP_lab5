03ab6c4d462512a68ba14565dad634daada74880
Zuucker <kamigie504@student.polsl.pl>
2020-11-05 12:14:30 +0100

    end to end example

--- application/application.cpp
+++ application/application.cpp
@@ -1,12 +1,16 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+    string input_file, output_file;
     cout << argc << endl;
     for (int i = 0; i < argc; i++)
     {
-        string input_file, output_file;
+
         if (string(argv[i]) == "-i")
         {
             input_file = argv[i + 1];
@@ -15,7 +19,32 @@ int main(int argc, char *argv[])
         {
             output_file = argv[i + 1];
         }
+    }
         cout << "input file: " << input_file << endl;
         cout << "output file: " << output_file << endl;
+
+        ifstream source(input_file);
+        vector<string> lines;
+        string tmp;
+        if (source.is_open())
+        {
+            while (getline(source, tmp))
+            {
+                lines.push_back(tmp);
+            }
+            source.close();
+        }
+
+    ofstream destination(output_file);
+
+    if (destination.is_open())
+    {
+        for (int i = 0; i < lines.size(); i++)
+        {
+            destination << lines[i];
+        }
     }
+
+
+    return 0;
 }
